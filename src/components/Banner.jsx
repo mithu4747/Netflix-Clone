78d3483e204849f50917e5a9cc5445f96e597163
mithu4747 <midlajmithu16744@gmail.com>
2023-09-28 12:27:07 +0530

    netflix design

---
+++ src/components/Banner.jsx
@@ -0,0 +1,34 @@
+import React,{useState, useEffect} from 'react'
+import './Banner.css';
+import tmdbAxiosInstance from '../tmdbAxiosInstance';
+
+function Banner({fetchUrl}) {
+    const [movie,setMovie] = useState()
+    const baseUrl = "https://image.tmdb.org/t/p/original/";
+
+    const fetchData = async ()=>{
+        const {data} = await tmdbAxiosInstance.get(fetchUrl)
+        console.log(data.results[Math.floor(Math.random()*data.results.length)]);
+        setMovie(data.results[Math.floor(Math.random()*data.results.length)])  
+    }
+    console.log(movie);
+
+    useEffect(()=>{
+        fetchData()
+    },[])
+  return (
+    <div style={{
+      height:'600px',
+      backgroundImage:`url(${baseUrl}/${movie?.backdrop_path})`,
+      backgroundSize:'cover',
+      backgroundAttachment:'fixed'
+    }}>
+      <div className='banner-content'>
+        <h1> {movie?.name}</h1>
+        <h2>{movie?.overview?.slice(0,200)}...</h2>
+      </div>
+       </div>
+  )
+}
+
+export default Banner
---
+++ src/components/Nav.jsx
@@ -0,0 +1,23 @@
+import React, {useState,useEffect} from 'react'
+import './Nav.css'
+
+function Nav() {
+    const  [show,setShow]= useState(false)
+    useEffect(()=>{
+        window.addEventListener("scroll",()=>{
+            if(window.scrollY>600){
+                setShow(true)
+            }else{
+                setShow(false)
+            }
+        })
+    })
+   // console.log(show);
+  return (
+    <div className={`${show && 'nav-black'} nav`}>
+        <img width={'150px'} src="https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png" alt="netflix logo" />
+    </div>
+  )
+}
+
+export default Nav
